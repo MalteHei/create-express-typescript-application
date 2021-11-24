1ea5b14dd4c553c0eb2bfcc93826edfa71f2eae7
Malte Heidemann <hei@ordix.de>
2021-11-24 23:44:18 +0100

    delete old templates

--- create-express-typescript-application-prisma-sample/src/index.ts
+++
@@ -1,7 +0,0 @@
-import app from './app';
-
-// Start the application by listening to specific port
-const port = Number(process.env.PORT || process.env.PORT || 8080);
-app.listen(port, () => {
-  console.info('Express application started on port: ' + port);
-});
