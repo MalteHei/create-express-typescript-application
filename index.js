6e2e9424261eb43613c047fb5010b5ca0552e248
Malte Heidemann <hei@ordix.de>
2021-11-25 00:46:28 +0100

    update index.js to reflect changes in helpers

--- index.js
+++ index.js
@@ -1,54 +1,28 @@
 #!/usr/bin/env node
 
-/**
- * Copyright (c) 2020-present, Prawira Genestonlia.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 'use strict';
 
 const { Command } = require('commander');
+const { version } = require('./package.json');
 const generateApp = require('./helper');
 
 const program = new Command();
-program.version('2.1.1');
-
-program
-    .option('-d, --debug', 'output extra debugging')
-    .option('-t, --template <template>', 'specify only prisma, plain or typeorm')
-    .option('-p, --prisma', 'define template prisma')
-    .option('-y, --typeorm', 'define template typeorm');
-
-
+program.version(version);
 program.parse(process.argv);
 
 const options = program.opts();
 if (options.debug) console.log(options);
 
 if (program.args.length !== 1) {
-    throw Error('Wrong number of arguments!');
+    console.error('Wrong number of arguments!');
+    process.exit(1);
 }
 
-let template = 'plain'
+console.log('Creating app...');
 
-if (options.prisma) {
-    template = 'prisma';
-} else if (options.typeorm) {
-    template = 'typeorm';
-} else if (options.template) {
-    if (options.template === 'prisma' || options.template === 'plain' || options.template === 'typeorm') {
-        template = options.template;
-    } else {
-        throw Error('Wrong template!');
-    }
-}
+const projectName = program.args[0];
 
-// Creating new project started
-console.log('Setting up new Express with typescript support app...');
-
-// Creating new project finished
-generateApp(program.args[0], template).then(() => {
-    console.log(`Application has been created with ${template} template!`);
+generateApp(projectName).then(() => {
+    console.log(`${projectName} has been created!`);
+    console.log(`Edit ${projectName}/src/index.ts to add routes to your application.`);
 });
